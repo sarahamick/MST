793a13bedc9612a9022ae7830697f25b9476399b
sarahamick <saram@itu.dk>
2018-10-30 22:57:18 +0100

    Added read-in efficiency improvements

--- MST.cpp
+++ MST.cpp
@@ -9,6 +9,12 @@
 #include<algorithm>
 using namespace std;
 
+struct Edge{
+  int v1;
+  int v2;
+  long weight;
+};
+
 //method to split a string by a specified delimiter
 vector<string> split(const string& s, char delimiter){
   vector<string> tokens;
@@ -20,8 +26,8 @@ vector<string> split(const string& s, char delimiter){
   return tokens;
 }
 
-bool sortCol(const vector<long>& v1, const vector<long>& v2){
-  return v1[2] < v2[2];
+bool sortCol(const Edge& v1, const Edge& v2){
+  return v1.weight < v2.weight;
 }
 
 long find(long* uf, long v){
@@ -49,8 +55,9 @@ bool connected(long* uf, long v1, long v2){
 }
 
 //method implementing the Kruskal's MST algorithm
-long mst(vector< vector<long> >* graph, int numE, int& numV){
+long mst(vector<Edge>* graph, int numE, int& numV){
   //1. Initialize the UnionFind array to be size of number vertices with each V the parent of itself
+
    long* uf = new long[numV];
    long* rank = new long[numV];
    for(int i = 0; i != numV; ++i) {
@@ -64,10 +71,10 @@ long mst(vector< vector<long> >* graph, int numE, int& numV){
   long v1;
   long v2;
   for(int i = 0; i != numE; ++i){
-    v1 = graph->at(i)[0];
-    v2 = graph->at(i)[1];
+    v1 = graph->at(i).v1;
+    v2 = graph->at(i).v2;
     if(!connected(uf, v1, v2)){
-      weight += graph->at(i)[2];
+      weight += graph->at(i).weight;
       unionVertices(uf, rank, v1, v2);
     }
   }
@@ -77,43 +84,47 @@ long mst(vector< vector<long> >* graph, int numE, int& numV){
 }
 
 //method to convert a text graph representation into a 2D array
-vector< vector<long> >* read_file(int n, string filename, int& numV){
+vector<Edge>* read_file(int n, string filename, int& numV){
 
-  vector< vector<long> >* graph = new vector< vector<long> >(n, vector<long>(3)); //create a pointer to a 2D array in memory:
+  vector<Edge>* graph = new vector<Edge>(n); //create a pointer to a 2D array in memory:
 
   int largestNode = 0;
-  long fromNode;
-  long toNode;
+  int fromNode;
+  int toNode;
   long weight;
-  string line;
-  ifstream myfile(filename);
-  if (myfile.is_open()){
-    int count = 0;
-    while(getline(myfile, line)){
-      vector<string> edge = split(line, ' ');
-      fromNode = stol(edge[0]);
-      toNode = stol(edge[1]);
-      weight = stol(edge[2]);
-      if(fromNode > largestNode) largestNode = fromNode;
-      if(toNode > largestNode) largestNode = toNode;
 
-      graph->at(count)[0] = fromNode;
-      graph->at(count)[1] = toNode;
-      graph->at(count)[2] = weight;
+  ifstream stream(filename);
+  char ch = stream.get();
+  int count = 0;
+  while(!stream.eof()){
+    fromNode = 0;
+    toNode = 0;
+    weight = 0;
 
-      count = count + 1;
-    }
-    myfile.close();
+    while(48 <= ch && ch <= 57) {fromNode = (fromNode * 10) + (ch - 48); ch = stream.get(); }
+    ch = stream.get(); //skip the space
+    while (48 <= ch && ch <= 57) {toNode = (toNode * 10) + (ch - 48); ch = stream.get();}
+    ch = stream.get(); //skip the space
+    while (48 <= ch && ch <= 57) {weight = (weight * 10) + (ch - 48); ch = stream.get();}
+    ch = stream.get(); //skip the new line
+
+    if(fromNode > largestNode) largestNode = fromNode;
+    if(toNode > largestNode) largestNode = toNode;
+
+    graph->at(count).v1 = fromNode;
+    graph->at(count).v2 = toNode;
+    graph->at(count).weight = weight;
+
+    count = count + 1;
   }
-  else cout << "Unable to open file \n";
-  numV = largestNode + 1;
 
+  numV = largestNode + 1;
   return graph;
 }
 
 int main(int argc, char** args){
-  int numV;
-  vector< vector<long> >* graph = read_file(atoi(args[1]), args[2], numV);
+  int numV = 0;
+  vector<Edge>* graph = read_file(atoi(args[1]), args[2], numV);
   long minWeight = mst(graph, atoi(args[1]), numV);
   delete graph;
   cout << minWeight << endl;
